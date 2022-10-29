5f93b3846796895404cfd95a8073bf6b97a66882
AbellaP <abellap2020@gmail.com>
2022-10-29 11:56:52 -0400

    Code Added

---
+++ lesson02/game.py
@@ -0,0 +1,18 @@
+import random
+print("Hello!")
+name = input("What is your name? ")
+print("Hello, " + name + ".")
+gender = input("What is your gender? (m(Male) or f(Female)) ")
+if gender == "m":
+    gender = "male"
+else:
+    gender = "female"
+f_guide = ["Sarah", "Ellie", "Ayla", "Eileen", "Helen"]
+m_guide = ["Chris", "Ryan", "Cameron", "Ethan", "Gordon"]
+guide_name = "name"
+index = random.randint(0, 4)
+if gender == "male":
+    guide_name = m_guide[index]
+else:
+    guide_name = f_guide[index]
+print("My name is " + guide_name + ".")
