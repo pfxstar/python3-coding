af232eff55a196b433cf6c8d57bad2e19bfa5c4c
AbellaP <abellap2020@gmail.com>
2022-11-12 11:38:10 -0500

    Added Lesson 04

---
+++ lesson04/string.py
@@ -0,0 +1,29 @@
+# string methods
+#name = input("What is your name? ")
+#name = name.capitalize()
+#print(name)
+
+#pet = input("Do you want a pet? ")
+#pet = pet.casefold()
+#print(pet)
+
+#txt = "blah"
+#txt = txt.center(30)
+#print(txt)
+
+#imnothere = "beautiful beautiful beautiful beautiful beautiful beautiful beautiful beautiful beautiful beautiful beautiful beautiful beautiful beautiful"
+#imnothere = imnothere.count("beautiful")
+#print(imnothere)
+
+#bye = "My dog is so cuteº"
+#bye = bye.encode()
+#print(bye)
+
+#poo = "Hello poops!"
+#poo = poo.endswith("!")
+#print(poo)
+
+#dadadadada = "fart haha you suck dumbhead."
+#dadadadada = dadadadada.find("bum")
+#print(dadadadada)
+
