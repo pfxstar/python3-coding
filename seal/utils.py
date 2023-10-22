0b48661dd2398c312199d224b902f0f12ec8ae65
AbellaP <abellap2020@gmail.com>
2023-10-22 11:26:50 -0400

    Learned Lists

---
+++ seal/utils.py
@@ -0,0 +1,3 @@
+def print_list(ls):
+    for value in ls:
+        print(value)
--- students.py
+++ students.py
@@ -1,4 +1,5 @@
-from seal import fibonacci, sorting
+# Imports
+from seal import fibonacci, sorting, utils
 from seal.math import calculus
 import random
 import math
@@ -103,7 +104,7 @@ print(sorting.bubble_sort([6, 2, 1, 3, 4, 5]))
 print(dir())
 print(calculus.hello()) '''
 
-# Python Random Module
+''' # Python Random Module
 my_list = ['a', 'b', 'c', 'd', 'e', 'f']
 
 print(random.randrange(10, 20))
@@ -133,4 +134,80 @@ print(math.floor(254238542452.245659))
 print(math.floor(-1.3))
 
 print(math.fabs(134))
-print(math.fabs(-134))
+print(math.fabs(-134)) '''
+
+# Lists
+my_list2 = ['a', 'b', 'c', 'd', 'e', 'f', 'a']
+
+my_list2.remove('a')
+print(my_list2)
+
+del my_list2[1]
+print(my_list2)
+
+last = my_list2.pop()
+print(my_list2)
+print(last)
+
+print(my_list2.index('f'))
+print(my_list2.count('d'))
+print(len(my_list2))
+
+my_list2.sort()
+print(my_list2)
+my_list2.sort(reverse=True)
+print(my_list2)
+
+my_list3 = ['g', 'a', 'c', 'd', 'f', 'e', 'b']
+
+my_list3.reverse()
+print(my_list3)
+my_list3.sort()
+print(my_list3)
+
+my_list4 = my_list3.copy()
+print(my_list4)
+
+my_list4.append('h')
+print(my_list4)
+print(my_list3)
+
+a = 3
+b = a
+b = 4
+print(a)
+
+c = 'etre'
+d = c
+d = 'avoir'
+print(c)
+
+print(min(my_list4))
+print(max(my_list4))
+
+utils.print_list(my_list4)
+
+print('z' in my_list4)
+
+# List Comprehensions
+my_list5 = [1, 5, 8, 3, 15, 66]
+
+''' 
+for index, value in enumerate(my_list5):
+    my_list5[index] *= 2 
+'''
+
+my_list6 = [i * 2 for i in my_list5]
+print(my_list6)
+
+my_list7 = [i + 5 for i in my_list5]
+print(my_list7)
+
+my_list8 = [i + 10 for i in my_list5 if i % 2 != 0]
+print(my_list8)
+
+my_list9 = [i if i % 2 == 0 else i + 10 for i in my_list5]
+print(my_list9)
+
+my_list10 = [i if i % 2 == 0 else i * 2 for i in my_list5]
+print(my_list10)
